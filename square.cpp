ecb300663d9dc8f59fa24ff833f1fb8d0b9c9018
poojamo1 <134801698+poojamo1@users.noreply.github.com>
2023-05-30 20:32:58 +0530

    Create square.cpp

---
+++ square.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 25; i++) {
+        int square = i * i;
+        cout << "Square of " << i << ": " << square << endl;
+    }
+
+    return 0;
+}
