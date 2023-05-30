4d49c0ac2ba386487ab37645adb8bd9bdd5b1877
poojamo1 <134801698+poojamo1@users.noreply.github.com>
2023-05-30 20:02:54 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,47 @@
+// Write a C program to implement a calculator using switch case statement
+
+#include <stdio.h>
+
+int main() {
+    int choice;
+    float num1, num2, result;
+
+    printf("Simple Calculator\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("Enter your choice (1-4): ");
+    scanf("%d", &choice);
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    switch (choice) {
+        case 1:
+            result = num1 + num2;
+            printf("Result: %.2f\n", result);
+            break;
+        case 2:
+            result = num1 - num2;
+            printf("Result: %.2f\n", result);
+            break;
+        case 3:
+            result = num1 * num2;
+            printf("Result: %.2f\n", result);
+            break;
+        case 4:
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("Result: %.2f\n", result);
+                } else {
+                printf("Error: Division by zero\n");
+            }
+            break;
+        default:
+            printf("Invalid choice\n");
+            break;
+    }
+
+    return 0;
+}
